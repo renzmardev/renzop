126ba8422e00e1a1fddd1644b3be76debd361d0d
Renzmar <renzmar.malabanan@essilor.com>
2026-07-14 15:20:38 +0800

    Remove all files

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// If you deploy to GitHub Pages at https://<user>.github.io/<repo>/,
-// set base to "/<repo>/". For a custom domain or Vercel/Netlify, leave it as "/".
-export default defineConfig({
-  plugins: [react()],
-  base: "/masters-besties-open-play/",
-});
